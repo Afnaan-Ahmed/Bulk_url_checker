943614dc887a963142b3ddd01775cec27d931541
Afnaan-Ahmed <afnaan2180@gmail.com>
2021-07-31 17:26:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+urls_file = open("urls.txt")
+content = urls_file.readlines()
+
+    
+
+def check_connectivity(url):
+    try:
+        a = urllib.request.urlopen(url).getcode()
+        print(f"{i}: Website is working.")
+
+    except:
+        print(f"{i}: Website is not working!")
+
+for i in content:
+    check_connectivity(i)
