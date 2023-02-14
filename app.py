b13d8dcdd0775e1aa0acd2c5a6c6f3d6f5e0f5c6
Afnaan <afnaan2180@gmail.com>
2023-02-15 00:25:33 +0530

    commit

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ urls_file.close()
 def check_connectivity(url):
     try:
         a = urllib.request.urlopen(url).getcode()
-        print(f"{i}: Website is working.")
+        print(f"{i}: Website is working.\n")
 
     except:
-        print(f"{i}: Website is not working!")
+        print(f"{i}: Website is not working!\n")
 
 for i in content:
     check_connectivity(i)
